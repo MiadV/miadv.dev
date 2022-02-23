094b3714e9d75b3e6212f0967f29810ce46b5396
miadv <miadsoft@gmail.com>
2022-02-23 14:44:48 +0800

    chore: format all files

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,6 +4,9 @@ import { widontString } from '@/utils/widontString';
 import SEO from '@/components/SEO';
 import Container from '@/layouts/Container';
 import Tag from '@/components/Tag';
+import GithubIcon from '@/Icons/GithubIcon';
+import YoutubeIcon from '@/Icons/YoutubeIcon';
+import ExternalLinkIcon from '@/Icons/ExternalLinkIcon';
 
 const mockProjects = [
   {
@@ -48,8 +51,7 @@ export default function Projects() {
           <ul className="space-y-8">
             {mockProjects.map((project) => (
               <li key={project.id}>
-                <div className="group relative flex flex-col overflow-hidden rounded-2xl bg-gray-200 shadow-xl transition-all duration-300 hover:-translate-y-0.5 hover:saturate-100">
-                  <div className="absolute top-2 left-2 z-10">actions</div>
+                <article className="group flex flex-col overflow-hidden rounded-2xl bg-gray-200 shadow-xl transition-all duration-300 hover:-translate-y-0.5 hover:shadow-2xl hover:saturate-100 dark:bg-gray-500">
                   <Image
                     src={project.thumbnail}
                     alt="project screenshot"
@@ -57,20 +59,57 @@ export default function Projects() {
                     height={720}
                     className="saturate-50 transition-all duration-300 group-hover:scale-[1.02] group-hover:saturate-100"
                   />
-                  <div className="bg-white p-3 dark:bg-slate-800">
+                  <footer className="relative space-y-2 bg-white p-3 dark:bg-slate-800">
+                    <div className="flex space-x-2">
+                      {project.youtube && (
+                        <a
+                          href={project.youtube}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                          className="fill-gray-600 hover:fill-indigo-600 dark:fill-gray-400 dark:hover:fill-indigo-400"
+                        >
+                          <span className="sr-only">{`watch on youtube`}</span>
+                          <YoutubeIcon />
+                        </a>
+                      )}
+                      {project.github && (
+                        <a
+                          href={project.github}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                          className="fill-gray-600 hover:fill-indigo-600 dark:fill-gray-400 dark:hover:fill-indigo-400"
+                        >
+                          <span className="sr-only">{`github repo`}</span>
+                          <GithubIcon />
+                        </a>
+                      )}
+
+                      {project.live && (
+                        <a
+                          href={project.live}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                          className="fill-gray-600 hover:fill-indigo-600 dark:fill-gray-400 dark:hover:fill-indigo-400"
+                        >
+                          <span className="sr-only">{`live website`}</span>
+                          <ExternalLinkIcon />
+                        </a>
+                      )}
+                    </div>
                     <h3
                       className="text-sm font-semibold text-gray-900 dark:text-gray-50 md:text-base"
                       title={project.title}
                     >
                       {project.title}
                     </h3>
-                    <div className="mt-2 space-x-1">
+
+                    <div className="space-x-1">
                       {project.tags.map((tag) => (
                         <Tag key={tag}>{tag}</Tag>
                       ))}
                     </div>
-                  </div>
-                </div>
+                  </footer>
+                </article>
               </li>
             ))}
           </ul>
