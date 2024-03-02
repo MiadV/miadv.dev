33380195e744b70079daf0084dd0325b7269de13
Miad Vosoughi <miadsoft@gmail.com>
2024-03-02 13:50:36 +0800

    Revert "Add 3d avatar"

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,18 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 
-export const Card: React.FC<{ children: ReactNode; className?: string }> = ({
+export const Card: React.FC<{ className?: string }> = ({
   children,
   className,
-}) => (
-  <div
-    className={clsx(
-      'rounded-2xl border border-slate-200 bg-white p-6 shadow-xl dark:border-slate-700 dark:bg-slate-800',
-      className
-    )}
-  >
-    {children}
-  </div>
-);
+}) => {
+  return (
+    <div
+      className={clsx(
+        'rounded-2xl border border-slate-200 bg-white p-6 shadow-xl dark:border-slate-700 dark:bg-slate-800',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
